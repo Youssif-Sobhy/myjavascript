d6be676677ae239223cae2d05761ef531bbb4471
youssif sobhy <youssifsobhy50@gmail.com>
2024-05-12 16:19:58 +0300

    routes with different responses using replace method (templating)

--- webserver.js
+++ webserver.js
@@ -6,8 +6,24 @@ htmlfile=fs.readFileSync('./templates/index.html','utf-8')
 
 const server=http.createServer((request,response)=>{
 
-    response.end(htmlfile)
-    console.log(" a new req received")
+
+    // response.end(htmlfile)
+
+    // console.log(" a new req received")
+
+    //to handle routes
+    let path=request.url;
+    console.log(path)
+    if (path.toLocaleLowerCase()==='/home' || path==='/'){
+        response.end(htmlfile.replace('{{%CONTENT%}}',"this is home page it will only show if you visit home page"));
+    }
+    else if (path==='/about'){
+        response.end(htmlfile.replace('{{%CONTENT%}}',"this is about page it will only show if you visit about page"));
+    }
+    else{
+        response.end("bad request");
+    }
+
 })
 
 
